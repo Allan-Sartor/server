d2f71a211227b8e6dfc3a5293ec2e70a0db13fd9
Allan-Sartor-Pontomais <allan.sartor@pontomais.com.br>
2024-05-24 00:47:47 -0300

    [Docs] for helpers

--- app/controllers/concerns/format_helper.rb
+++ app/controllers/concerns/format_helper.rb
@@ -1,17 +1,48 @@
-# Módulo FormatHelper
-# Fornece funções auxiliares para formatação de mensagens e serialização de um objeto em formato JSON
+# Módulo: FormatHelper
+#
+# O módulo `FormatHelper` fornece funções auxiliares para formatação de mensagens e serialização de objetos em formato JSON.
+# Este módulo é protegido e deve ser incluído em classes que necessitam dessas funcionalidades.
+
 module FormatHelper
   protected
 
+  # Método: serializer_as_json
+  #
+  # Este método serializa um objeto utilizando o ActiveModelSerializers e o retorna no formato JSON.
+  #
+  # Params:
+  # - render: O objeto que será serializado.
+  #
+  # Returns:
+  # - JSON: O objeto serializado em formato JSON.
   def serializer_as_json(render)
     ActiveModelSerializers::SerializableResource.new(render).as_json
   end
 
+  # Método: render_message_i18n
+  #
+  # Este método traduz uma mensagem de acordo com as configurações de internacionalização (I18n).
+  #
+  # Params:
+  # - message: A chave da mensagem que será traduzida.
+  #
+  # Returns:
+  # - String: A mensagem traduzida.
   def render_message_i18n(message)
     I18n.t(message)
   end
 
+  # Método: render_error_message
+  #
+  # Este método renderiza uma mensagem de erro no formato JSON.
+  #
+  # Params:
+  # - message: A chave da mensagem de erro que será traduzida.
+  # - status: O código de status HTTP que será retornado na resposta.
+  #
+  # Returns:
+  # - JSON: Um objeto JSON contendo a mensagem de erro traduzida.
   def render_error_message(message, status)
-    render json: { error: render_message(message) }, status: status
+    render json: { error: render_message_i18n(message) }, status: status
   end
 end
