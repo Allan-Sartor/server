ed8bea5a2acfd040af4d88b2511b6af3e532d7df
Allan-Sartor-Pontomais <allan.sartor@pontomais.com.br>
2024-05-24 00:33:03 -0300

    [Setup] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :contacts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Configuração das rotas.
+#
+# As rotas definidas com o formato padrão JSON.
+# As rotas utilizam o Devise Token Auth para autenticação do usuário e incluem rotas para gerenciar contatos.
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+require 'api_constraints'
+
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    # Monta as rotas do Devise Token Auth para o modelo User no caminho /api/auth
+    mount_devise_token_auth_for 'User', at: 'auth'
+
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: false) do
+      # Monta as rotas do Devise Token Auth para o modelo User no caminho /api/delete_account
+      # Utiliza o controller customizado para registros localizado em api/v1/registrations
+      mount_devise_token_auth_for 'User', at: 'delete_account', controllers: {
+        registrations: 'api/v1/registrations'
+      }
+
+      # Define as rotas padrão CRUD (Create, Read, Update, Delete) para o recurso contacts
+      resources :contacts
+    end
+  end
 end
