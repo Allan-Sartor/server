622dd512624870fbd6fa4ab6a447d4b15c129371
Allan-Sartor-Pontomais <allan.sartor@pontomais.com.br>
2024-05-24 00:36:23 -0300

    [Docs] for class User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# Classe da tabela de usuários gerada via devise
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   has_many :contacts, dependent: :destroy
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 end
