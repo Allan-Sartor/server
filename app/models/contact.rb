ed8508cee154f37aa348b6242b06406f61af0ba1
Allan-Sartor-Pontomais <allan.sartor@pontomais.com.br>
2024-05-24 22:34:27 -0300

    [Feat] included sort by for contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,14 +2,22 @@
 # Inclui validações para garantir a presença e unicidade de atributos como CPF, nome, telefone, endereço, latitude e longitude.
 # Também realiza a validação do CPF com base em cálculos de dígitos verificadores.
 class Contact < ApplicationRecord
+  include Sortable
+
   belongs_to :user
 
   include CpfValidator
 
-  validates :cpf, presence: true, uniqueness: { scope: :user_id }
+  validates :cpf, presence: true, uniqueness: { scope: :user_id }, on: :create
   validates :name, presence: true
   validates :phone, presence: true
   validates :address, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+
+  def self.sort_by
+    ['cpf', 'name']
+  end
+
+  private_class_method :sort_by
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 # Classe da tabela de usuários gerada via devise
 class User < ApplicationRecord
-  has_many :contacts, dependent: :destroy
+  include DeviseTokenAuth::Concerns::User
 
+  # Include default devise modules.
   devise :database_authenticatable,
          :registerable,
          :recoverable,
          :rememberable,
          :validatable
+
+  has_many :contacts, dependent: :destroy
 end
